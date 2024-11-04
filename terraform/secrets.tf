b1b0dc907a934682303dc598810121fcb738ab2d
Dialgatrainer02 <oliviabarker900@gmail.com>
2024-11-04 20:35:40 +0000

    use override files

---
+++ terraform/secrets.tf
@@ -0,0 +1,40 @@
+variable "pve_username" {
+  type    = string
+  default = ""
+}
+
+variable "pve_password" {
+  type    = string
+  default = ""
+}
+
+variable "pve_endpoint" {
+  type    = string
+  default = ""
+}
+
+
+variable "tenancy_ocid" {
+  default = ""
+}
+
+variable "user_ocid" {
+  default = ""
+}
+
+variable "fingerprint" {
+  default = ""
+}
+
+variable "compartment_ocid" {
+  default = ""
+}
+
+variable "private_key_path" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
