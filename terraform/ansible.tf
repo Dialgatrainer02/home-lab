d83ee67c3d2f304539c5140bf5b8ac8d759ebdd0
Dialgatrainer02 <oliviabarker900@gmail.com>
2024-11-14 12:24:32 +0000

    use terraform rescource over path

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -9,8 +9,8 @@ locals {
     "all" = {
       "children" = {
         for group in local.groups : group => {
-          hosts = { for host in keys(local.hosts) : host => {/*...*/} if contains(local.hosts[host].ansible_groups, group) 
-          #  host => { 
+          hosts = { for host in keys(local.hosts) : host => { /*...*/ } if contains(local.hosts[host].ansible_groups, group)
+            #  host => { 
             # "ansible_host" = "${try(oci_core_instance.wireguard_instance[host].public_ip, trimsuffix(proxmox_virtual_environment_vm.almalinux_vm[host].initialization[0].ip_config[0].ipv4[0].address, "/24"), trimsuffix(proxmox_virtual_environment_container.almalinux_container[host].initialization[0].ip_config[0].ipv4[0].address, "/24"))}"
             # "ansible_user" = "${contains(keys(var.oracle), host) ? "opc" : contains(keys(var.vms), host) ? "almalinux" : "root"}"
             # if contains(local.hosts[host],"ansible_variables") ? for k,v in host.ansible_variables: k => {v} : ""    figure out how to place ansiblr vars in here
@@ -19,7 +19,7 @@ locals {
         }
       }
       "vars" = {
-        "ansible_ssh_private_key_file" = "./terraform/homelab_key"
+        "ansible_ssh_private_key_file" = local_sensitive_file.homelab_key.filename
       }
     }
   })
@@ -32,8 +32,8 @@ output "inventory" {
 
 
 resource "local_file" "inventory" {
-    content = local.inventory
-    filename = "./terraform-inventory.yml"
+  content  = local.inventory
+  filename = "./terraform-inventory.yml"
 }
 
 
