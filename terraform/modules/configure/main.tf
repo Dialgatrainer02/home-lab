5ddda7cfb92a4968db38a0dcba30677f4e85ab4f
Dialgatrainer02 <oliviabarker900@gmail.com>
2024-12-22 20:36:50 +0000

    make things sensitive

--- terraform/modules/configure/main.tf
+++ terraform/modules/configure/main.tf
@@ -1,4 +1,4 @@
-resource "local_file" "extra_vars" {
+resource "local_sensitive_file" "extra_vars" {
   filename = "${path.module}/extra_vars.yml"
   content  = "--- \n${yamlencode(var.extra_vars)}" # alittle hacky but ultimatly fine
 }
@@ -9,7 +9,7 @@ resource "local_file" "inventory" {
   filename = "${path.root}/../ansible/inventory.yml"
   content  = yamlencode(var.inventory)
   provisioner "local-exec" {
-    command     = "ansible-playbook ${var.playbook} -i ${local_file.inventory.filename} -e \"@${local_file.extra_vars.filename}\" "
+    command     = "ansible-playbook ${var.playbook} -i ${local_file.inventory.filename} -e \"@${local_sensitive_file.extra_vars.filename}\" "
     working_dir = path.root
     environment = {
       ANSIBLE_HOST_KEY_CHECKING = var.host_key_checking
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,50 +1,60 @@
 variable "pve_username" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "pve_password" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "pve_address" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 
 variable "tenancy_ocid" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "user_ocid" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "fingerprint" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "compartment_ocid" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "private_key" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "region" {
   type    = string
   default = ""
+  sensitive = true
 }
 
 variable "duckdns_token" {
   type    = string
   default = ""
+  sensitive = true
 }
